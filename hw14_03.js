6af97fd8e6d831d9c625a5b2a78e939512336826
TimurRashitov <timurrrashitov@gmail.com>
2025-08-14 11:37:53 +0200

    addTask3

--- hw14_03.js
+++ hw14_03.js
@@ -3,6 +3,54 @@
 */
 
 function largestValueInArray(array, func) {
-
-    
+    if (typeof(func) === "function"){
+        return func(array);
+    } else {
+        return "Error";
+    }
 }
+
+
+function comparator(arr) {
+    if (Array.isArray(arr) && arr.length > 0) {
+        let stringResult = arr[0];
+        for (let element of arr) {
+            if( typeof(element) === "string" && element.length >= stringResult.length) {
+                stringResult = element;
+            }
+        }
+        return stringResult;
+    } else {
+        return "Ошибка данных. Нет результата с самой длинной строкой"
+    }
+}
+
+function createArray() {
+
+    let userInput = prompt("Введите строки, разделяя их знаком ; (Пример ввода: Tom;Maxim;Bob;Jack;Ekaterina)")
+
+    if (userInput !== null && userInput.trim() !== "") {
+
+        let arrayUser = userInput.split(";");
+        console.log(arrayUser);
+
+        let arrayStrings = [];
+        for (let element of arrayUser) {
+            element = element.replace(/["',]/g, "");
+            arrayStrings.push(element.trim());
+        }
+        console.log(arrayStrings);
+        return arrayStrings;
+
+    } else {
+        console.log("Ошибка ввода!");
+        return [];
+    }
+}
+
+const createdArrFromUser = createArray();
+console.log(createdArrFromUser);
+
+let largestString = largestValueInArray(createdArrFromUser, comparator);
+console.log(largestString);
+
